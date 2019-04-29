cecaeb52f7a21240b6954409a1018499c8c5c3d5
HyunSukKang <21400018@handong.edu>
2019-04-29 17:39:37 +0900

    commit

---
+++ src/main/java/edu/handong/csee/java/inheritance/Cat.java
@@ -0,0 +1,26 @@
+package edu.handong.csee.java.inheritance;
+
+public class Cat extends Animal {
+	
+	// hiding
+    public static void testClassMethod() {
+        System.out.println("The static method in Cat");
+    }
+	
+	// overriding
+    public void testInstanceMethod() {
+        System.out.println("The instance method in Cat");
+    }
+
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        myCat.testClassMethod();
+        
+        Animal myAnimal = myCat;
+        Animal.testClassMethod();
+        myAnimal.testInstanceMethod();
+        
+        Animal myDog = new Dog(); // polymorphism
+        myDog.testInstanceMethod();
+    }
+}
