31e3405f596de8fa0faa3fd8f7c018eaeedf5c5a
HyunSukKang <21400018@handong.edu>
2019-04-30 13:06:22 +0900

    Cat.java comment

--- src/main/java/edu/handong/csee/java/inheritance/Cat.java
+++ src/main/java/edu/handong/csee/java/inheritance/Cat.java
@@ -2,12 +2,12 @@ package edu.handong.csee.java.inheritance;
 
 public class Cat extends Animal {
 	
-	// hiding
+    // hiding : superclass에서 정의된 static method를 재정의해서 사용하는 것을 hiding 이라고 한다.  
     public static void testClassMethod() {
         System.out.println("The static method in Cat");
     }
 	
-    // overriding
+    // overriding : superclass에서 정의된 method를 재정의해서 사용하는 것을 overriding 이라고 한다. 
     public void testInstanceMethod() {
         System.out.println("The instance method in Cat");
     }
@@ -20,7 +20,7 @@ public class Cat extends Animal {
         Animal.testClassMethod();
         myAnimal.testInstanceMethod(); // overriding
         
-        Animal myDog = new Dog(); // polymorphism
+        Animal myDog = new Dog(); // polymorphism : Animal 타입으로 선언 된 myAnimal, myDog이 Cat이 될 수도, Dog이 될 수도 있다. 이것을 polymorphism이라고 한다. 
         myDog.testInstanceMethod();
     }
 }
