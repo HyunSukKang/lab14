d9827b2107678a89ef756cc55a8d47e5ffc1bacd
HyunSukKang <21400018@handong.edu>
2019-05-10 12:08:23 +0900

    hi

--- src/main/java/edu/handong/csee/java/inheritance/Dog.java
+++ src/main/java/edu/handong/csee/java/inheritance/Dog.java
@@ -1,5 +1,5 @@
 package edu.handong.csee.java.inheritance;
-
+// hi for commit 2019. 5. 10
 public class Dog extends Animal {
 	public void testInstanceMethod() {
         System.out.println("The instance method in Dog");
